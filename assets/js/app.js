f3e282922ca30ee100c8b60241942200ccb1901a
chakler <arias17f@gmail.com>
2021-06-07 22:33:05 -0500

    Primer diseño para móvil

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+const toggle = (elementId) => {
+  const element = document.getElementById(elementId);
+
+    element.classList.toggle(elementId + "--open");
+
+//   if (!element.classList.contains(elementId + "--open")) {
+//     element.classList.add(elementId + "--open");
+//   } else {
+//     element.classList.remove(elementId + "--open");
+//   }
+};
+
+const toggleNavigation = () => {
+  toggle("nav");
+  toggle("hamburguer-button");
+};
